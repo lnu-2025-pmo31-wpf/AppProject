158d7542b9d23ee918b0aa19944f55ec5db90e7f
Владислав Барабалюк <vlad251520@gmail.com>
2025-12-11 19:58:48 +0200

    add smth

---
+++ Presentation/Converters/AmountColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using FinanceManager.DAL.Entities;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FinanceManager.Presentation.Converters
+{
+    public class AmountColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Якщо це сума (decimal) або тип транзакції, перевіряємо логіку
+            // Тут припускаємо, що ми біндимося до самого об'єкта Transaction, щоб знати Type
+            // Але простіше біндити колір до TransactionType
+
+            if (value is TransactionType type)
+            {
+                return type == TransactionType.Income ? Brushes.Green : Brushes.Red;
+            }
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
